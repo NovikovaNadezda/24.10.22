b5274baafaad8e23926ae0a141dce2e42a88e82d
NovikovaNadezda <114093562+NovikovaNadezda@users.noreply.github.com>
2022-10-24 18:28:47 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*int n;
+	cin >> n;
+	for (int i = 1; i <= n; i += 1) {
+		cout << "@";
+	}*/
+	//дано 2 числа х и у найти сумму четных чисел из диапазона х-у
+	/*int x, y, i, sum = 0;
+	cin >> x;
+	cin >> y;
+	i = x;
+	while (i <= y) {
+		if (i % 2 == 0) {
+			sum += i;
+		}
+		i++;
+	}
+	cout << sum << endl;*/
+     //напечатать нечетные числа из диапазона х-у в пор€дке убывани€
+	/*int x;
+	int y;
+	cin >> x;
+	cin >> y;
+	for (int i = 1; i <= y; i += 1) {
+		if (i % 2 == 0) {
+			i++;
+		}
+		cout << i;
+	}*/
+	/*//напечатать пр€моугольник 10х8 из символов #*/
+	
+	/*for (int i = 0; i <20; i++) cout <<' #';
+		cout << endl;
+		for (int i = 0; i < 6; i++)
+		{
+			cout << '#';
+			for (int j = 0; i < 18; j++) cout << ' ';
+			cout << '#' << endl;
+		}
+	for (int i = 0; i < 20; i++) cout << ' #';
+	cout << endl;*/
+	/*int n;
+	cin >> n;
+	int i;
+	int proisv;
+	while (i <= n) {
+		if (i % 3 == 0) {
+			proisv * i;
+		}
+		i++;
+	}cout << proisv;*/
+	//18. «адано натуральное число N.Ќайти количество натуральных чисел, не превосход€щих N и не дел€щихс€ ни на одно из чисел 2, 3, 5.
+	int n;
+
+	cin >> n;
+
+}
